45a2ca98802a06469dedf7a20945e8b2fcbd43c0
Laye Yomba IFONO <layeyomba12@gmail.com>
2025-02-17 14:02:10 +0000

    Gestion des composants et le routing

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { Routes } from '@angular/router';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+   { path: 'produits', component: ProduitsComponent },
+   {  path: 'addproduit', component: AddProduitComponent},
+   {path: '', redirectTo: 'produits', pathMatch: 'full'},
+   {path: '**',  component: PageNotFoundComponent}
+
+];
