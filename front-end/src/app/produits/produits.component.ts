a6a278b4b755382ba701e5ff207207ca4dcd361c
Laye Yomba IFONO <layeyomba12@gmail.com>
2025-02-17 19:31:44 +0000

    Finalisation du formulaire des produits

--- front-end/src/app/produits/produits.component.ts
+++ front-end/src/app/produits/produits.component.ts
@@ -1,18 +1,48 @@
-
 import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './produits.component.html',
-  styleUrl: './produits.component.css'
+  styleUrl: './produits.component.css',
 })
 export class ProduitsComponent {
-
-  produits : string[];
+  produits: Produit[];
 
   constructor() {
-    this.produits = ['Livre', 'Cahier', 'Stylo', 'Gomme'];
+    this.produits = [
+      {
+        idProduit: 1,
+        nomProduit: 'PC Asus',
+        prixProduit: 3000000,
+        dateCreation: new Date('01/14/2011'),
+      },
+      {
+        idProduit: 2,
+        nomProduit: 'Imprimante Epson',
+        prixProduit: 15000000,
+        dateCreation: new Date('12/17/2010'),
+      },
+      {
+        idProduit: 3,
+        nomProduit: 'Tablette Samsung',
+        prixProduit: 7000000,
+        dateCreation: new Date('02/20/2020'),
+      },
+      {
+        idProduit: 4,
+        nomProduit: 'PC Asus',
+        prixProduit: 5000000,
+        dateCreation: new Date('01/14/2011'),
+      },
+      {
+        idProduit: 5,
+        nomProduit: 'PC Asus',
+        prixProduit: 5000000,
+        dateCreation: new Date('01/14/2011'),
+      },
+    ];
   }
-
 }
