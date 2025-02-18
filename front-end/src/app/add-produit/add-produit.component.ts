d61512f7cda79a8a4c2256d28c0d8b87d0a18db5
Laye Yomba IFONO <layeyomba12@gmail.com>
2025-02-18 15:31:38 +0000

    Formulaire pour ajouter des produits

--- front-end/src/app/add-produit/add-produit.component.ts
+++ front-end/src/app/add-produit/add-produit.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
 
 @Component({
   selector: 'app-add-produit',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-produit.component.html',
   styleUrl: './add-produit.component.css'
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
+ 
+  // Déclaration des variables pour le formulaire
+  message : string = "";
+
+  newProduit = new Produit();
+  constructor(private produitService : ProduitService) {
+    // Injection du service ProduitService
+
+   }
+
+
+  ngOnInit(): void {
+    // Initialisation du composant
+  }
+
+  // Méthode pour ajouter un produit
+  addProduit(){
+
+     this.produitService.ajouterProduit(this.newProduit);
+    this.message = "Produit: " + this.newProduit.nomProduit + " ajouté avec succès !";
+   
+
+  }
 
 }
--- front-end/src/app/produits/produits.component.ts
+++ front-end/src/app/produits/produits.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
@@ -8,41 +9,20 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css',
 })
-export class ProduitsComponent {
-  produits: Produit[];
+export class ProduitsComponent implements OnInit {
+ 
+  produits : Produit[]=[];
 
-  constructor() {
-    this.produits = [
-      {
-        idProduit: 1,
-        nomProduit: 'PC Asus',
-        prixProduit: 3000000,
-        dateCreation: new Date('01/14/2011'),
-      },
-      {
-        idProduit: 2,
-        nomProduit: 'Imprimante Epson',
-        prixProduit: 15000000,
-        dateCreation: new Date('12/17/2010'),
-      },
-      {
-        idProduit: 3,
-        nomProduit: 'Tablette Samsung',
-        prixProduit: 7000000,
-        dateCreation: new Date('02/20/2020'),
-      },
-      {
-        idProduit: 4,
-        nomProduit: 'PC Asus',
-        prixProduit: 5000000,
-        dateCreation: new Date('01/14/2011'),
-      },
-      {
-        idProduit: 5,
-        nomProduit: 'PC Asus',
-        prixProduit: 5000000,
-        dateCreation: new Date('01/14/2011'),
-      },
-    ];
+  constructor(private produitService : ProduitService) {
+      
   }
+
+
+  ngOnInit(): void {
+    this.produits = this.produitService.listProduit();
+  }
+
+
+
+
 }
